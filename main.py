5308ea6de802201ec01d8c21ddd56caaef8b90a7
pasumaibharatam <pasumaibharatam@gmail.com>
2026-01-16 18:54:19 +0530

    bck

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://pol-ui.onrender.com"
+        "https://pasumaibharatam.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
